6fe4c07512ca5360eab81f0fee90bed830e0272b
Anastasiia Lytvynenko <152660862+apovshr@users.noreply.github.com>
2025-12-24 15:55:56 -0700

    Update lab10_first.py

--- lab10_first.py
+++ lab10_first.py
@@ -2,7 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(0,10, 50)
+
+#рівняння
 y = (5 * np.cos(10*x) * np.sin(3*x)) / (x ** (1/2))
+
+#граф
 plt.plot(x, y, label= 'функція у', color = 'deepskyblue', linewidth = 3)
 plt.title('Перший граф', fontsize = 19, color = 'cadetblue')
 plt.xlabel('Вісь х', color = 'darkolivegreen', fontsize = 12)
